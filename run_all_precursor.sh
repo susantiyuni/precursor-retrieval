4c5553e361d4208937d52be62b4d626996fdefe5
susantiyuni <36025078+susantiyuni@users.noreply.github.com>
2026-04-09 17:55:41 +0200

    Update run_all_precursor.sh

--- run_all_precursor.sh
+++ run_all_precursor.sh
@@ -9,9 +9,9 @@ set -e
 # chmod +x run_all_precursor.sh
 
 # Run the Python script for different temporal prior functions
-python src/run-all-precursor.py --out_dir output-all --temp gamma
+python src/run-all-precursor.py --out_dir output-gamma --temp gamma
 
-# python src/run-all-precursor.py --out_dir output-all --temp beta
+# python src/run-all-precursor.py --out_dir output-beta --temp beta
 # python src/run-all-precursor.py --out_dir output-decay --temp decay
 # python src/run-all-precursor.py --out_dir output-gaussian --temp gaussian
 # python src/run-all-precursor.py --out_dir output-laplace --temp laplace
