2adb4781fa91171e6da3da848f7f1379c7686301
susantiyuni <36025078+susantiyuni@users.noreply.github.com>
2026-05-07 15:54:38 +0200

    Update run_all_zs.sh

--- run_all_zs.sh
+++ run_all_zs.sh
@@ -10,6 +10,8 @@ set -e
 
 # fixed configurations, example:
 python src/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl --temp gamma --abl metadata --trace hybrid_topical_continuity
+python src/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl --temp decay --abl all --trace int_influence
+python src/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl --temp lognormal --abl citation --trace comm_consensus
 
 # running all ablation + fix temporal mode
 python src/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl --temp gamma
@@ -22,3 +24,6 @@ python src/run_zs.py --out_dir runs_zs_abltrace --trace int_influence --temp gam
 python src/run_zs.py --out_dir runs_zs_abltrace --trace comm_consensus --temp gamma --abl base
 python src/run_zs.py --out_dir runs_zs_abltrace --trace pure_topical_continuity --temp gamma --abl base
 python src/run_zs.py --out_dir runs_zs_abltrace --trace hybrid_topical_continuity --temp gamma --abl base
+
+# fixed temporal, on base model, using all traces
+python src/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl --temp decay --abl base 
