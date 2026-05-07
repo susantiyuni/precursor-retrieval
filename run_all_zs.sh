2846bc121f747e0c71381aa62f6e023ce73d69a7
susantiyuni <36025078+susantiyuni@users.noreply.github.com>
2026-05-07 15:21:51 +0200

    Update run_all_zs.sh

--- run_all_zs.sh
+++ run_all_zs.sh
@@ -8,4 +8,11 @@ set -e
 # Make it executable
 # chmod +x run_all_zs.sh
 
-python src-ltr/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl
+# default configurations
+python src/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl
+
+# running on schema ablations
+python src/run_zs.py --out_dir runs_zs_abltrace --trace int_influence --temp gamma --abl base
+python src/run_zs.py --out_dir runs_zs_abltrace --trace comm_consensus --temp gamma --abl base
+python src/run_zs.py --out_dir runs_zs_abltrace --trace pure_topical_continuity --temp gamma --abl base
+python src/run_zs.py --out_dir runs_zs_abltrace --trace hybrid_topical_continuity --temp gamma --abl base
