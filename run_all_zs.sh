07d519a1821c7ac936ce085b0def34737b53195d
susantiyuni <36025078+susantiyuni@users.noreply.github.com>
2026-05-07 15:50:55 +0200

    Update run_all_zs.sh

--- run_all_zs.sh
+++ run_all_zs.sh
@@ -8,14 +8,17 @@ set -e
 # Make it executable
 # chmod +x run_all_zs.sh
 
-# default configurations
+# fixed configurations, example:
+python src/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl --temp gamma --abl metadata --trace hybrid_topical_continuity
+
+# running all ablation + fix temporal mode
+python src/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl --temp gamma
+
+# running all ablation + all temporal ablations
 python src/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl
 
 # running on schema/trace ablations
 python src/run_zs.py --out_dir runs_zs_abltrace --trace int_influence --temp gamma --abl base
-
 python src/run_zs.py --out_dir runs_zs_abltrace --trace comm_consensus --temp gamma --abl base
-
 python src/run_zs.py --out_dir runs_zs_abltrace --trace pure_topical_continuity --temp gamma --abl base
-
 python src/run_zs.py --out_dir runs_zs_abltrace --trace hybrid_topical_continuity --temp gamma --abl base
