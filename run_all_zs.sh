58646ddce5afe403b86874a7d5868e0336f18426
susantiyuni <36025078+susantiyuni@users.noreply.github.com>
2026-05-07 15:19:32 +0200

    Update run_all_zs.sh

--- run_all_zs.sh
+++ run_all_zs.sh
@@ -9,4 +9,3 @@ set -e
 # chmod +x run_all_zs.sh
 
 python src-ltr/run_zs.py --out_dir runs_zs --inputf data/candidate-pool.jsonl --feat data/sparql_feats.jsonl
-python src-ltr/run_zs_baseline.py --out_dir runs_zs_baseline --inputf data/candidate-pool.jsonl 
